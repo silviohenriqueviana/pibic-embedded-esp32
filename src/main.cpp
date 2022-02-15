634851e4995601c2fefd2b58eb33e7880523a71c
silviohenriqueviana <silvio.viana@ufpe.br>
2022-02-15 13:36:27 -0300

    Receive date

--- src/main.cpp
+++ src/main.cpp
@@ -1,133 +1,116 @@
 #include <Arduino.h>
 #include "LoRa_E220.h"
+#include <Wire.h>
+#include <SPI.h>
+#include <RTClib.h>
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 // ---------- esp32 pins --------------
 LoRa_E220 e220ttl(&Serial2, 15, 21, 19, UART_BPS_RATE_9600);
 
-void printParameters(struct Configuration configuration);
-void reset();
-void printModuleInformation(struct ModuleInformation moduleInformation);
-
-struct Message {
-	char type[5];
-	char message[8];
-	float temperature;
-};
-
-struct SensorData
-{
-	int typeMsg = 0;
-	int id = 1;
-	float tempCell = 1;
-	float tempExternal = 0;
-	float tempInternal = 0;
-	int current = 0;
-	int date = 0;
-};
-
-SensorData packet;
-
-void setup(){
-	Serial.begin(9600);
-    while (!Serial) {
-        ; // wait for serial port to connect. Needed for native USB
-    }
-    delay(100);
- 
-	e220ttl.begin();
-
-	// After set configuration comment set M0 and M1 to low
-	//    and reboot if you directly set HIGH M0 and M1 to program
-	ResponseStructContainer c;
-    c = e220ttl.getConfiguration();
+void config(){
+    ResponseStructContainer c;
+	c = e220ttl.getConfiguration();
     Configuration configuration = *(Configuration*) c.data;
     configuration.ADDL = 0x47;
-    configuration.ADDH = 0X51;
-    configuration.CHAN = 42;
-
+	configuration.ADDH = 0x51;
+   	configuration.CHAN = 42;
 	configuration.SPED.uartBaudRate = UART_BPS_9600; // Serial baud rate
 	configuration.SPED.airDataRate = AIR_DATA_RATE_010_24; // Air baud rate
 	configuration.SPED.uartParity = MODE_00_8N1; // Parity bit
-
 	configuration.OPTION.subPacketSetting = SPS_200_00; // Packet size
 	configuration.OPTION.RSSIAmbientNoise = RSSI_AMBIENT_NOISE_DISABLED; // Need to send special command
 	configuration.OPTION.transmissionPower = POWER_22; // Device power
-
-    configuration.TRANSMISSION_MODE.enableRSSI = RSSI_DISABLED; // Enable RSSI info
-    configuration.TRANSMISSION_MODE.fixedTransmission = FT_FIXED_TRANSMISSION; // Enable repeater mode
-    configuration.TRANSMISSION_MODE.enableLBT = LBT_DISABLED; // Check interference
-    configuration.TRANSMISSION_MODE.WORPeriod = WOR_2000_011; // WOR timing
-
-
+   	configuration.TRANSMISSION_MODE.enableRSSI = RSSI_DISABLED; // Enable RSSI info
+   	configuration.TRANSMISSION_MODE.fixedTransmission = FT_FIXED_TRANSMISSION; // Enable repeater mode
+   	configuration.TRANSMISSION_MODE.enableLBT = LBT_DISABLED; // Check interference
+   	configuration.TRANSMISSION_MODE.WORPeriod = WOR_2000_011; // WOR timing
     e220ttl.setConfiguration(configuration, WRITE_CFG_PWR_DWN_SAVE);
-    printParameters(configuration);
+}
 
-
-	Serial.println();
-    Serial.println("Start listening!");
+struct Message {
+    char type[5];
+    char message[8];
+    byte temperature[4];
 };
 
-void loop(){
-	if (e220ttl.available()  > 1){
-		ResponseStructContainer rsc = e220ttl.receiveMessage(40);
-        Serial.println(rsc.status.getResponseDescription());
-		//packet = *(SensorData*) rsc.data;
-		//reset();
-		//packet = *(SensorData*) rsc.data;
+void setup() {
+  Serial.begin(9600);
+  delay(500);
 
-		//Serial.println(packet.tempCell);
-		//Serial.println(packet.tempExternal);
-		//Serial.println(packet.tempInternal);
-		//Serial.println(packet.date);
-		//Serial.println(packet.current);
-		//Serial.println(packet.typeMsg);
-		//Serial.println(packet.id);
-    }
+  // Startup all pins and UART
+  e220ttl.begin();
+  config();
 }
 
-void printParameters(struct Configuration configuration) {
-	DEBUG_PRINTLN("----------------------------------------");
+typedef struct
+{
+  	uint16_t typeMsg : 16;
+  	uint16_t id : 16;
+  	uint16_t current : 16;
+  	uint16_t tempCell : 16;
+  	uint16_t tempExternal : 16;
+  	uint16_t tempInternal : 16;
+  	uint32_t date : 32;
+} packetTypeSensorsData;
 
-	DEBUG_PRINT(F("HEAD : "));  DEBUG_PRINT(configuration.COMMAND, HEX);DEBUG_PRINT(" ");DEBUG_PRINT(configuration.STARTING_ADDRESS, HEX);DEBUG_PRINT(" ");DEBUG_PRINTLN(configuration.LENGHT, HEX);
-	DEBUG_PRINTLN(F(" "));
-	DEBUG_PRINT(F("AddH : "));  DEBUG_PRINTLN(configuration.ADDH, HEX);
-	DEBUG_PRINT(F("AddL : "));  DEBUG_PRINTLN(configuration.ADDL, HEX);
-	DEBUG_PRINTLN(F(" "));
-	DEBUG_PRINT(F("Chan : "));  DEBUG_PRINT(configuration.CHAN, DEC); DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.getChannelDescription());
-	DEBUG_PRINTLN(F(" "));
-	DEBUG_PRINT(F("SpeedParityBit     : "));  DEBUG_PRINT(configuration.SPED.uartParity, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.SPED.getUARTParityDescription());
-	DEBUG_PRINT(F("SpeedUARTDatte     : "));  DEBUG_PRINT(configuration.SPED.uartBaudRate, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.SPED.getUARTBaudRateDescription());
-	DEBUG_PRINT(F("SpeedAirDataRate   : "));  DEBUG_PRINT(configuration.SPED.airDataRate, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.SPED.getAirDataRateDescription());
-	DEBUG_PRINTLN(F(" "));
-	DEBUG_PRINT(F("OptionSubPacketSett: "));  DEBUG_PRINT(configuration.OPTION.subPacketSetting, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.OPTION.getSubPacketSetting());
-	DEBUG_PRINT(F("OptionTranPower    : "));  DEBUG_PRINT(configuration.OPTION.transmissionPower, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.OPTION.getTransmissionPowerDescription());
-	DEBUG_PRINT(F("OptionRSSIAmbientNo: "));  DEBUG_PRINT(configuration.OPTION.RSSIAmbientNoise, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.OPTION.getRSSIAmbientNoiseEnable());
-	DEBUG_PRINTLN(F(" "));
-	DEBUG_PRINT(F("TransModeWORPeriod : "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.WORPeriod, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getWORPeriodByParamsDescription());
-	DEBUG_PRINT(F("TransModeEnableLBT : "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.enableLBT, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getLBTEnableByteDescription());
-	DEBUG_PRINT(F("TransModeEnableRSSI: "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.enableRSSI, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getRSSIEnableByteDescription());
-	DEBUG_PRINT(F("TransModeFixedTrans: "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.fixedTransmission, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getFixedTransmissionDescription());
+typedef union packetSensorsData {
+  	unsigned char encoded[16];
+  	packetTypeSensorsData decoded;
+} packetSensorsData;
 
+void loop() {
+	// If something available
+  if (e220ttl.available()>1) {
+	  // read the String message
+		ResponseStructContainer rsc = e220ttl.receiveMessage(16);
 
-	DEBUG_PRINTLN("----------------------------------------");
-}
-void printModuleInformation(struct ModuleInformation moduleInformation) {
-	Serial.println("----------------------------------------");
-	DEBUG_PRINT(F("HEAD: "));  DEBUG_PRINT(moduleInformation.COMMAND, HEX);DEBUG_PRINT(" ");DEBUG_PRINT(moduleInformation.STARTING_ADDRESS, HEX);DEBUG_PRINT(" ");DEBUG_PRINTLN(moduleInformation.LENGHT, DEC);
+	// Is something goes wrong print error
+	if (rsc.status.code!=1){
+		Serial.println(rsc.status.getResponseDescription());
+	}else{
+		// Print the data received
+		Serial.println(rsc.status.getResponseDescription());
+		packetSensorsData message = *(packetSensorsData*) rsc.data;
+		uint16_t typeMsg =static_cast<uint16_t>(message.decoded.typeMsg);
+		uint16_t id =static_cast<uint16_t>(message.decoded.id);
+		uint16_t corrente =static_cast<uint16_t>(message.decoded.current);
+		uint16_t celula = static_cast<uint16_t>(message.decoded.tempCell);
+		uint16_t externa = static_cast<uint16_t>(message.decoded.tempExternal);
+		uint16_t interna = static_cast<uint16_t>(message.decoded.tempInternal);
+		uint32_t data = static_cast<uint32_t>(message.decoded.date);
+		Serial.print("TypeMSG: ");
+		Serial.println(typeMsg);
+		Serial.print("ID: ");
+		Serial.println(id);
+		Serial.print("Corrente: ");
+		Serial.println(corrente);
+		Serial.print("Temperatura da celula: ");
+		Serial.println(celula);
+		Serial.print("Temperatura da externa: ");
+		Serial.println(externa);
+		Serial.print("Temperatura da interna: ");
+		Serial.println(interna);
+		Serial.print("Data: ");
+		Serial.println(data);
+		
+		DateTime now(data);
+		Serial.print(now.year(), DEC);
+    	Serial.print('/');
+    	Serial.print(now.month(), DEC);
+    	Serial.print('/');
+    	Serial.print(now.day(), DEC);
+    	Serial.print(" (");
+    	Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    	Serial.print(") ");
+    	Serial.print(now.hour(), DEC);
+    	Serial.print(':');
+    	Serial.print(now.minute(), DEC);
+    	Serial.print(':');
+    	Serial.print(now.second(), DEC);
+    	Serial.println();
 
-	Serial.print(F("Model no.: "));  Serial.println(moduleInformation.model, HEX);
-	Serial.print(F("Version  : "));  Serial.println(moduleInformation.version, HEX);
-	Serial.print(F("Features : "));  Serial.println(moduleInformation.features, HEX);
-	Serial.println("----------------------------------------");
-
-}
-
-void reset(){
-	packet.current       = 10;
-	packet.date          = 20;
-	packet.tempCell      = 30;
-	packet.tempExternal  = 2;
-	packet.tempInternal  = 4;
-	packet.id            = 60;
-	packet.typeMsg       = 70;
+	}
+  }
 }
